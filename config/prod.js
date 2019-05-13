ec87b8db2da10ad1d8d190f0eac39b029dea781b
Pedro Nogueira <17pedro.n@gmail.com>
2019-05-13 16:43:53 +0100

    added Keys config for dev and prod

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod keys
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID//'589658927997-h70les2f02gpb6gigsjdp5hftnh3297d.apps.googleusercontent.com',
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET//'1q5wanhLBbv1BZVqzYUwIsVh',
+  mongoURI: process.env.MONGO_URI//'mongodb+srv://emailyProdAdmin:4F8KcDlyZrrqX52U@emaily-prod-z8qpo.mongodb.net/test?retryWrites=true',
+  cookieKey: process.env.COOKIE_KEY//'qwASD$12x..(0mo'
+};
