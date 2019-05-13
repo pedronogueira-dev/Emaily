18deed212c6e155a348b43e692bbc076b54004b3
Pedro Nogueira <17pedro.n@gmail.com>
2019-05-13 15:59:31 +0100

    Finished implementing Authentication via Google Oauth

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ require('./models/User');
 require('./services/passport'); //Configuration of google authentication
 const express = require('express');
 const mongoose = require('mongoose');
-
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-
 const keys = require('./config/keys');
-
-
 const app = express();
+
+const COOKIE_EXPIRATION = 30 * 24 * 60 * 60 * 1000;
 app.use(
   cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    maxAge: COOKIE_EXPIRATION,
     keys: [keys.cookieKey]
   })
 );
