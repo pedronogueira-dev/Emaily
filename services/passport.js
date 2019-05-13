d72cf7971006d0686091c0154c2ce9de867d87b7
Pedro Nogueira <17pedro.n@gmail.com>
2019-05-13 17:11:25 +0100

    Added auth/google callback url as an env var

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,7 @@ passport.deserializeUser((userId,done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: keys.googleCallbackURL
   },
   (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleId: profile.id })
